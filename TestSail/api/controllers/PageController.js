5a4a7d30ca52c2e0e5bc40f385b9a459bac9dab9
Balavigneswaran Kuppusamy <bala.vigneswaran@gmail.com>
2017-02-11 05:04:40 -0600

    Minor changes..... Done over many days..

---
+++ TestSail/api/controllers/PageController.js
@@ -0,0 +1,29 @@
+module.exports = {
+  showHomePage: function(req, res) {
+    sails.log.info('Session is ... ', req.session.me);
+    if(!req.session.me) {
+      return res.view('homepage');
+    }
+
+    Person.findOne(req.session.me, function(err, u) {
+      if(err) return res.negotiate(err);
+      if(!u) {
+        sails.log.warn('User id on the session is invalid... ', req.session.me);
+        return res.view('homepage');
+      }
+
+      return res.view('dashboard', {
+        me: {
+          id: u.id,
+          firstName: u.firstName,
+          lastName: u.lastName,
+          email: u.email,
+          lastLoggedIn: u.lastLoggedIn
+        }
+      });
+
+    });
+
+
+  }
+}
--- TestSail/api/controllers/UserController.js
+++ TestSail/api/controllers/UserController.js
@@ -2,11 +2,87 @@ module.exports = {
   _config: {
     actions: false
   },
+
   index: function(req, resp) {
     return resp.view('mypg', {
       date: (new Date()).toString(),
       age: 25,
       place: 'Houston'
     });
+  },
+
+  signup: function(req, res) {
+    sails.log.silly('UserController.signup() ::  Trying to signup - ' + req.param('email'));
+    var Passwords = require('machinepack-passwords');
+    // Encrypt a string using the BCrypt algorithm.
+    Passwords.encryptPassword({
+      password: req.param('password'),
+      difficulty: 10
+    }).exec({
+      error: function(err) {
+        sails.log.error('There is some problem in the encryption...', err);
+        res.negotiate(err);
+      },
+      success: function(pwd) {
+        Person.create({
+          firstName: req.param('firstName'),
+          lastName: req.param('lastName'),
+          email: req.param('email'),
+          password: pwd,
+          lastLoggedIn: new Date()
+        }, function created(err, newUser) {
+          if(err) {
+            sails.log.error('UserController.signup() ::  Error in creating Person : ', err);
+            // console.log('Error attributes...' + err.invalidAttributes);
+            res.invalidInput();
+            // res.negotiate(err);
+          }
+          req.session.me = newUser.id;
+          // console.log('Person created...' + newUser);
+          return res.json(newUser);
+        });
+      },
+    });
+  },
+
+  login: function(req, res) {
+    sails.log.silly('UserController.login() ::  Trying to login - ' + req.param('email'));
+    Person.findOne({
+      email: req.param('email')
+    }, function usr(err, u) {
+      if(err) return res.negotiate(err);
+      if(!u) return res.notFound();
+      require('machinepack-passwords').checkPassword({
+        passwordAttempt: req.param('password'),
+        encryptedPassword: u.password
+      }).exec({
+        error: function(err) {
+          sails.log.error('There is some problem in the decryption...', err);
+          res.negotiate(err);
+        },
+        incorrect: function() {
+          sails.log.info('Password does not match...');
+          res.notFound();
+        },
+        success: function() {
+          sails.log.silly('Password matched!!!! ...');
+          req.session.me = u.id;
+          res.ok();
+        }
+      });
+    });
+  },
+
+  logout: function(req, res) {
+    sails.log.silly('UserController.logout() ::  Trying to logout - ' + req.session.me);
+    Person.findOne(req.session.me, function usr(err, u) {
+      if(err) return res.negotiate(err);
+      if(!u) {
+        sails.log.warn('Session refers to user who does not exist...', err);
+      }
+      req.session.me = null;
+      return res.backToHomePage();
+    });
   }
+
 };
