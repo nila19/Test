9b36659f93c294abc633aaa40164f88ba0cc2a5c
Bala Kuppusamy <bala.vigneswaran@gmail.com>
2017-04-12 01:40:21 -0500

    Adding TestBulma

---
+++ TestBulma/bin/server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const http = require('http');
+
+const app = require('./app');
+const server = http.createServer(app);
+const port = 3300;
+
+app.set('port', port);
+server.listen(port);
